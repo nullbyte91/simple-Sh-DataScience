fa52a5418ab15379977086d2fb09bb23af4461ed
nullbyte91 <nullbyte.in@gmail.com>
2018-12-21 11:35:32 +0530

    syntax error issue, Printf changes and updated to do

--- amazon_configuration.sh
+++ amazon_configuration.sh
@@ -9,13 +9,19 @@ function configureAWS()
 	echo "To get a list of Region list:https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html"
 	read -r -p "Enter AWS CLI UserName?[Y/N]" UserName
 	aws configure --profile $UserName
+	echo "amazon CLI details:"
+	cat ~/.aws/config
+	cat ~/.aws/credentials
+	
+	#To:do
+	#Verify the AMS CLI with cloud
 }
 echo "#### Basic ubuntu update"
 # Update the apt package index and Upgrade the Ubuntu system
 sudo apt-get update && sudo apt-get -y upgrade
 
 #Install PIP if already not installed
-echo "#### Install Git"
+echo "#### Install PIP"
 for pkg in $PIP; do
     if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
         echo -e "$pkg is already installed"
@@ -53,7 +59,3 @@ read -r -p "Do you wanna configure AWS CLI?[Y/N]" response
 	else
     		echo "Assume you have done the Git configuration already"
 	fi
-}
-
-	
-

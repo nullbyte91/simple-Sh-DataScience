bd05d5776d50e912c5a1443de07f4209421886e8
nullbyte91 <nullbyte.in@gmail.com>
2018-12-26 12:03:58 +0530

    Common helper function script added. Implemented: Basic debian update, Debian package update and pip package update

---
+++ helper_function.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Copyright (c) 2018 Jegathesan Shanmugam
+# Released under the MIT License (MIT)
+# https://github.com/nullbyte91/Simple-Sh-DataScience/blob/master/LICENSE.md
+
+# Title           : helper_function.sh
+# Description     : This script contains helper function to support other bash scripts
+# author		  : Jegathesan Shanmugam
+
+function systemBasicUpdates()
+{
+    # Update the apt package index and Upgrade the Ubuntu system
+	sudo apt-get update 
+	if [[ $? > 0 ]]
+	then
+    	echo "apt-get update failed, exiting."
+    	exit
+	else
+    	echo "apt-get update ran succesfuly, continuing with script."
+	fi
+	sudo apt-get -y upgrade
+	if [[ $? > 0 ]]
+	then
+    	echo "apt-get upgrade failed, exiting."
+    	exit
+	else
+    	echo "apt-get upgrade ran succesfuly, continuing with script."
+	fi
+}
+
+function installAptPackages()
+{
+    for pkg in $1; do
+        	if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
+                	echo -e "$pkg is already installed"
+		else
+			if sudo apt-get -qq install $pkg; then
+    				echo "Successfully installed $pkg"
+			else
+    				echo "Error installing $pkg"
+			fi
+		fi
+	done
+}
+
+function installPipPackage()
+{
+    echo "$1 & $2"
+    if [ "$1" == "2" ];
+        then
+        pip install $2 --user
+    else  
+        pip3 install $2 --user
+    fi
+}

997301ae8c886dece729680e1e02634a4a734c2b
nullbyte91 <nullbyte.in@gmail.com>
2019-11-07 15:44:14 +0530

    Bug-fix: find a file compiled dep package and install

--- git-openssl.sh
+++ git-openssl.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-	
-sudo apt-get install -y build-essential fakeroot dpkg-dev
-sudo apt-get -y build-dep git
-sudo apt-get install -y libcurl4-openssl-dev
+function installDep()
+{
+    sudo apt-get install -y build-essential fakeroot dpkg-dev
+    sudo apt-get -y build-dep git
+    sudo apt-get install -y libcurl4-openssl-dev
+}
 
-mkdir git-openssl
-cd git-openssl
-apt-get source git
-cd git-*
-sed -i -e 's/libcurl4-gnutls-dev/libcurl4-openssl-dev/g' ./debian/control
-sed -i -- '/TEST\s*=\s*test/d' ./debian/rules
-sudo dpkg-buildpackage -rfakeroot -b
-sudo dpkg -i git_2.7.4-0ubuntu1.6_arm64.deb
+function compileGit()
+{
+    mkdir -p git-openssl
+    apt-get source git
+    cd git-*
+    sed -i -e 's/libcurl4-gnutls-dev/libcurl4-openssl-dev/g' ./debian/control
+    sed -i -- '/TEST\s*=\s*test/d' ./debian/rules
+    sudo dpkg-buildpackage -rfakeroot -b
+    find .. -type f -name "git_*ubuntu*.deb" -exec sudo dpkg -i \{\} \;
+}
 
-#CleanUp
-cd ../../
-sudo rm -rf git-openssl
+#Main
+#dep install
+installDep
 
-#Set git set on hold from apt-get/dpkg update
+# compile git with openssl
+compileGit
+
+# Put git on hold from software update
 sudo apt-mark hold git
-#Ref:
-#https://devopscube.com/gnutls-handshake-failed-aws-codecommit/
-
-
-
-
-
-

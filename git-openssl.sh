dc1c1f8df2b37d32a596da28eb4c1101e48b2ddb
nullbyte91 <nullbyte.in@gmail.com>
2019-11-14 00:09:23 +0530

    #2 Cleanup of packages used for building git #2

--- git-openssl.sh
+++ git-openssl.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 function installDep()
 {
     sudo apt-get install -y build-essential fakeroot dpkg-dev
@@ -10,12 +9,26 @@ function installDep()
 function compileGit()
 {
     mkdir -p git-openssl
+    cd git-openssl
     apt-get source git
     cd git-*
     sed -i -e 's/libcurl4-gnutls-dev/libcurl4-openssl-dev/g' ./debian/control
     sed -i -- '/TEST\s*=\s*test/d' ./debian/rules
     sudo dpkg-buildpackage -rfakeroot -b
     find .. -type f -name "git_*ubuntu*.deb" -exec sudo dpkg -i \{\} \;
+    cd ../../
+}
+
+function cleanUp()
+{
+    mkdir -p ../gitopenssl_deb
+    pushd git-openssl
+    # copy the package for future use
+    find .. -type f -name "git_*ubuntu*.deb" -exec cp {} ../../gitopenssl_deb \;
+    popd
+    
+    # Cleanup the compilation env
+    sudo rm -rf git-openssl
 }
 
 #Main
@@ -27,3 +40,6 @@ compileGit
 
 # Put git on hold from software update
 sudo apt-mark hold git
+
+# cleanUp
+cleanUp
